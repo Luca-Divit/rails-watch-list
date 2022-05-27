60dbc69d21b7a601f83595ef3257a5d585f6d58f
Luca Di Vittorio <luca.div@hotmail.com>
2022-05-27 12:52:04 +0100

    path changed and added buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
